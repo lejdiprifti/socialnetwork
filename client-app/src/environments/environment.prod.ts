503b85d36e2f963e99b8d663dbc15d9745e0da00
lejdiprifti <priftilejdi@gmail.com>
2020-01-14 22:02:08 +0100

    changing the name of social net

--- client-app/src/environments/environment.prod.ts
+++ client-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-export const BASE_URL: string = 'http://localhost:8080/social-network/api/';
+export const BASE_URL: string = 'http://localhost:8082/social-network/api/';
 
 
 export const environment = {
